72a265db55400aca8f7d261eb0797bd5ac0eaca8
alttaf hussain <alttaf.hussain@gmail.com>
2015-01-04 14:04:31 +0000

    % colours working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // app.js
 var app = angular.module('myApp', ['ngGrid']);
 app.controller('MyCtrl', function($scope, $http) {
-
+    $scope.x= console.log;
+    // serpatate into service
     var feedReader = function (data, titlesArray){
         var feedArray = [];
         var feedArray1 = [];
@@ -27,20 +28,17 @@ app.controller('MyCtrl', function($scope, $http) {
             feedArray1.push(row1);
             // push
         }
-        console.log(feedArray);
-        console.log(feedArray1);
+        //console.log(feedArray);
+        //console.log(feedArray1);
         $scope.feedArray = feedArray1;
         $scope.myData = feedArray1;
     }
      $scope.tArray =[ "gsx$ticker", "gsx$industry", "gsx$marketcap",  "gsx$price",  "gsx$change" , "gsx$volume"];    
      
-     $scope.tArray2 =[ {cellClass:"gsx$ticker", field:'gsx$ticker', displayName:'Ticker'}, {cellClass:"gsx$industry", field:'gsx$industry', displayName:'Industry'}, {cellClass:"gsx$marketcap", field:'gsx$marketcap', displayName:'Market Cap'},  {cellClass:"gsx$price", field:'gsx$price', displayName:'Price'},  {cellClass:"gsx$change",field:'gsx$change', displayName:'Change' }, {cellClass:"gsx$volume", field:'gsx$volume', displayName:'Volume' }];
+     $scope.tArray2 =[ {headerClass: 'gsx$tickerHead', cellClass:"gsx$ticker", field:'ticker', displayName:'Ticker'}, {cellClass:"gsx$industry", field:'industry', displayName:'Industry'}, {cellClass:"gsx$marketcap", field:'marketcap', displayName:'Market Cap'},  {cellClass:"gsx$price", field:'price', displayName:'Price'},  {cellClass:"gsx$change",field:'change', displayName:'Change',  cellTemplate: '<div ng-class="((row.getPropertyNumber(col.field))>=0)? \' green\':\' red\'"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'}, {cellClass:"gsx$volume", field:'volume', displayName:'Volume' }];
 
    $http.get('https://spreadsheets.google.com/feeds/list/18kEw2WXq6uhT9C0OE7uvocDVRahm245eAEpl3AVdl_4/o8ol38h/public/values?alt=json')
   .success(function (data, status) {
-    console.log("status"+status);
-    console.log("data"+data);
-    console.log(data);
     feedReader(data, $scope.tArray);
    // $scope.myData = $scope.feedArray1;
   })
@@ -49,10 +47,11 @@ app.controller('MyCtrl', function($scope, $http) {
     console.log("data"+data);
   });
 
-
+    console.log($scope.myData);
     $scope.gridOptions = { 
       data: 'myData', 
-      enableCellSelection: true,
-      //columnDefs: $scope.tArray2 
+      enableCellSelection: false,
+      enableRowSelection: false,
+      columnDefs: 'tArray2' 
     };
 });
